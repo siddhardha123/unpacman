e63edbcbf031bf3c935c66940fe3561cceff1487
siddhardha123 <siddhardha123123@gmail.com>
2025-01-17 23:54:33 +0530

    add mb support

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ async function getDependencySizes(packageJsonPath: string): Promise<void> {
   let totalUnpackedSize = 0;
 
   console.log(chalk.bold("Calculating unpacked sizes of dependencies...\n"));
-  console.log(chalk.cyan.bold("Package Name") + "  |  " + chalk.cyan.bold("Unpacked Size (KB)"));
+  console.log(chalk.cyan.bold("Package Name") + "  |  " + chalk.cyan.bold("Unpacked Size"));
   console.log(chalk.cyan("-".repeat(40)));
 
   for (const [dep, version] of Object.entries(dependencies)) {
@@ -31,7 +31,13 @@ async function getDependencySizes(packageJsonPath: string): Promise<void> {
       totalUnpackedSize += unpackedSize;
 
       const sizeInKB = unpackedSize / 1024;
-      const sizeDisplay = sizeInKB.toFixed(2) + " KB";
+      const sizeInMB = sizeInKB / 1024;
+      let sizeDisplay = `${sizeInKB.toFixed(2)} KB`;
+
+      // Display in MB if the size is greater than 1024 KB
+      if (sizeInMB >= 1) {
+        sizeDisplay = `${sizeInMB.toFixed(2)} MB`;
+      }
 
       // Colorize output based on size
       if (sizeInKB > 500) {
@@ -49,7 +55,16 @@ async function getDependencySizes(packageJsonPath: string): Promise<void> {
   }
 
   console.log(chalk.cyan("-".repeat(40)));
-  console.log(chalk.bold(`Total Unpacked Size: ${chalk.magenta.bold((totalUnpackedSize / 1024).toFixed(2))} KB`));
+  const totalSizeInKB = totalUnpackedSize / 1024;
+  const totalSizeInMB = totalSizeInKB / 1024;
+  let totalSizeDisplay = `${totalSizeInKB.toFixed(2)} KB`;
+
+  // Display total size in MB if greater than 1024 KB
+  if (totalSizeInMB >= 1) {
+    totalSizeDisplay = `${totalSizeInMB.toFixed(2)} MB`;
+  }
+
+  console.log(chalk.bold(`Total Unpacked Size: ${chalk.magenta.bold(totalSizeDisplay)}`));
 }
 
 async function calculateTarballSize(tarballStream: NodeJS.ReadableStream): Promise<number> {
